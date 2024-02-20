3ffced172a22d2f4641d776ef2e4eb7676390961
Wojciech <weswojciech@gmail.com>
2024-02-20 14:16:27 +0100

    make build

---
+++ index.322db375.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")}),document.querySelector(".form__button").addEventListener("click",function(e){e.preventDefault()},!1);
+//# sourceMappingURL=index.322db375.js.map
